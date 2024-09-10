5e4ff49d3b662b97dce09878c51e1cdd5772c841
Paul Anderson <pauldevop@gmail.com>
2024-09-09 23:53:47 -0300

    initial commit with project done

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import { Header } from "./components/Header";
+import { Post, PostType } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from './App.module.css';
+
+import './global.css';
+
+const posts: PostType[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/paulfms.png',
+      name: 'Paul Anderson',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-02-07 14:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-02-07 14:00:00'),
+  },
+];
+
+export function App() {
+  return (
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                post={post}
+              />
+            )
+          })}
+        </main>
+
+      </div>
+    </>
+  )
+}
